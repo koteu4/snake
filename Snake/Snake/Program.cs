9db6760102c0aae1b25457a9dd318187a3725798
koteu4 <koteu4@gmail.com>
2016-07-08 12:02:05 +0300

    lesson 2

---
+++ Snake/Snake/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Draw(1, 3, '*');
+            Draw(4, 5, '#');
+            
+            Console.ReadLine ();
+        }
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+
+    }
+}
